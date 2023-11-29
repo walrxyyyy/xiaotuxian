4ce579efab92261b5c5da6986a56d113f3810ffe
GSC <walrxyyyy1122@gmail.com>
2023-11-29 15:17:18 +0800

    静态资源引入和安装err len插件

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-import './assets/main.css'
+// 系统默认css
+// import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -6,9 +7,11 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-
+// 导入自己需要的初始化样式文件css
+import '@/styles/common.scss'
 
 const app = createApp(App)
+
 
 app.use(createPinia())
 app.use(router)
