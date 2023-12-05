399435d42e4553773d6251d101ce680e35c14226
GSC <walrxyyyy1122@gmail.com>
2023-12-05 16:23:30 +0800

    Home-面板组件封装

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -4,6 +4,7 @@ import HomeNew from "@/views/home/components/HomeNew.vue";
 import HomeBanner from "@/views/home/components/HomeBanner.vue";
 import HomeCategory from "@/views/home/components/HomeCategory.vue";
 import HomeProduct from "@/views/home/components/HomeProduct.vue";
+import HomePanel from "@/views/home/components/HomePanel.vue";
 </script>
 
 
@@ -15,6 +16,17 @@ import HomeProduct from "@/views/home/components/HomeProduct.vue";
     <HomeNew></HomeNew>
     <HomeHot></HomeHot>
     <home-product></home-product>
+    <!-- 测试面板封装 -->
+    <HomePanel title="新鲜好物" sub-title="新鲜好物 非常实惠">
+        <div>
+            这是一条测试数据
+        </div>
+    </HomePanel>
+    <HomePanel title="人气推荐" sub-title="人气推荐 非常实惠">
+        <div>
+            这是两条测试数据
+        </div>
+    </HomePanel>
 </template>
 
 <style scoped lang="scss">
