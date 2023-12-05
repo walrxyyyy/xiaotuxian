398f158c336e38f423610639be8f51ff42addfec
GSC <walrxyyyy1122@gmail.com>
2023-12-05 20:04:33 +0800

    图片懒加载实现

--- src/main.js
+++ src/main.js
@@ -9,6 +9,8 @@ import router from './router'
 
 // 导入自己需要的初始化样式文件css
 import '@/styles/common.scss'
+// 定义懒加载插件
+import {useIntersectionObserver} from '@vueuse/core'
 
 const app = createApp(App)
 
@@ -16,4 +18,21 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 
+app.directive('img-lazy',{
+    mounted(el,binding){
+        // el: 指令绑定的那个元素 img
+        // binding: binding.value  指令等于号后面绑定的表达式的值  图片url 
+        const {stop}=useIntersectionObserver(el,([{isIntersecting}])=>{
+            console.log(isIntersecting);
+            if(isIntersecting){
+                // 进入视口区域
+                el.src=binding.value
+                stop()
+            }
+        })
+    }
+})
+
 app.mount('#app')
+
+
