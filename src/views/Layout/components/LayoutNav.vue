c504a725a4d1dc257cde598bdc23ffc816bc0734
GSC <walrxyyyy1122@gmail.com>
2023-12-25 14:52:32 +0800

    登录-整体认识和路由配置

--- src/views/Layout/components/LayoutNav.vue
+++ src/views/Layout/components/LayoutNav.vue
@@ -7,7 +7,8 @@
     <nav class="app-topnav">
       <div class="container">
         <ul>
-          <template v-if="true">
+          <!-- 多模板渲染  区分登录状态和非登录状态 -->
+          <template v-if="false">
             <li><a href=""><i class="iconfont icon-iconrenxiang"></i>周杰伦</a></li>
             <li>
               <el-popconfirm title="确认退出吗?" confirm-button-text="确认" cancel-button-text="取消">
@@ -20,7 +21,7 @@
             <li><a href="">会员中心</a></li>
           </template>
           <template v-else>
-            <li><a href="">请先登录</a></li>
+            <li><a href="" @click="$router.push('/login')">请先登录</a></li>
             <li><a href="">帮助中心</a></li>
             <li><a href="">关于我们</a></li>
           </template>
