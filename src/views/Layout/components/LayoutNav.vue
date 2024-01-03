8984758b7ac83d874f1eff2d18d321473ccab98d
GSC <walrxyyyy1122@gmail.com>
2024-01-03 15:58:54 +0800

    登录-表单校验实现

--- src/views/Layout/components/LayoutNav.vue
+++ src/views/Layout/components/LayoutNav.vue
@@ -1,67 +1,71 @@
 <script setup>
 
+// 
+
 </script>
 
 
 <template>
-    <nav class="app-topnav">
-      <div class="container">
-        <ul>
-          <!-- 多模板渲染  区分登录状态和非登录状态 -->
-          <template v-if="false">
-            <li><a href=""><i class="iconfont icon-iconrenxiang"></i>周杰伦</a></li>
-            <li>
-              <el-popconfirm title="确认退出吗?" confirm-button-text="确认" cancel-button-text="取消">
-                <template #reference>
-                  <a href="">退出登录</a>
-                </template>
-              </el-popconfirm>
-            </li>
-            <li><a href="">我的订单</a></li>
-            <li><a href="">会员中心</a></li>
-          </template>
-          <template v-else>
-            <li><a href="" @click="$router.push('/login')">请先登录</a></li>
-            <li><a href="">帮助中心</a></li>
-            <li><a href="">关于我们</a></li>
-          </template>
-        </ul>
-      </div>
-    </nav>
-  </template>
+  <nav class="app-topnav">
+    <div class="container">
+      <ul>
+        <!-- 多模板渲染  区分登录状态和非登录状态 -->
+        <template v-if="false">
+          <li><a href=""><i class="iconfont icon-iconrenxiang"></i>周杰伦</a></li>
+          <li>
+            <el-popconfirm title="确认退出吗?" confirm-button-text="确认" cancel-button-text="取消">
+              <template #reference>
+                <a href="">退出登录</a>
+              </template>
+            </el-popconfirm>
+          </li>
+          <li><a href="">我的订单</a></li>
+          <li><a href="">会员中心</a></li>
+        </template>
+        <template v-else>
+          <li><a href="" @click="$router.push('/login')">请先登录</a></li>
+          <li><a href="">帮助中心</a></li>
+          <li><a href="">关于我们</a></li>
+        </template>
+      </ul>
+    </div>
+  </nav>
+</template>
   
   
-  <style scoped lang="scss">
-  .app-topnav {
-    background: #333;
-    ul {
-      display: flex;
-      height: 53px;
-      justify-content: flex-end;
-      align-items: center;
-      li {
-        a {
-          padding: 0 15px;
-          color: #cdcdcd;
-          line-height: 1;
-          display: inline-block;
-  
-          i {
-            font-size: 14px;
-            margin-right: 2px;
-          }
-  
-          &:hover {
-            color: $xtxColor;
-          }
+<style scoped lang="scss">
+.app-topnav {
+  background: #333;
+
+  ul {
+    display: flex;
+    height: 53px;
+    justify-content: flex-end;
+    align-items: center;
+
+    li {
+      a {
+        padding: 0 15px;
+        color: #cdcdcd;
+        line-height: 1;
+        display: inline-block;
+
+        i {
+          font-size: 14px;
+          margin-right: 2px;
         }
-  
-        ~li {
-          a {
-            border-left: 2px solid #666;
-          }
+
+        &:hover {
+          color: $xtxColor;
+        }
+      }
+
+      ~li {
+        a {
+          border-left: 2px solid #666;
         }
       }
     }
   }
-  </style>
+}
+</style>
